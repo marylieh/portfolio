302c9ffc758a684d5dcc6581de00bcaf0a0a7ae8
marylieh <m.aschmann@icloud.com>
2025-08-17 19:34:43 +0200

    Modify sentry config

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -8,8 +8,11 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://94958d99c12bda5335c2775a34e3f3d4@o1094435.ingest.us.sentry.io/4507788803964928",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
